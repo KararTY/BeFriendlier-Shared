31dc21c37fcba69a107ac8e889594df5f82dee5d
Karar Al-Remahy <alremahykarar@ymail.com>
2020-07-26 15:17:11 +0200

    feat(1.0.0): Shared files for BeFriendlier.

---
+++ dist/SocketTypes.js
@@ -0,0 +1,23 @@
+"use strict";
+exports.__esModule = true;
+var SocketMessage;
+(function (SocketMessage) {
+    SocketMessage["WELCOME"] = "W";
+    SocketMessage["JOINCHAT"] = "JC";
+    SocketMessage["LEAVECHAT"] = "LC";
+    SocketMessage["MATCH"] = "M";
+    SocketMessage["UNMATCH"] = "UM";
+    SocketMessage["ROLLMATCH"] = "RM";
+    SocketMessage["SUCCESS"] = "S";
+    SocketMessage["ADDEMOTES"] = "AE";
+    SocketMessage["ERROR"] = "ERR";
+    SocketMessage["UNREGISTERED"] = "UR";
+    SocketMessage["TAKEABREAK"] = "TAB";
+})(SocketMessage = exports.SocketMessage || (exports.SocketMessage = {}));
+var More;
+(function (More) {
+    More["NONE"] = "NONE";
+    More["BIO"] = "BIO";
+    More["FAVORITEEMOTES"] = "FE";
+    More["FAVORITESTREAMERS"] = "FS";
+})(More = exports.More || (exports.More = {}));
---
+++ src/SocketTypes.ts
@@ -0,0 +1,43 @@
+export enum SocketMessage {
+  WELCOME = 'W',
+  JOINCHAT = 'JC',
+  LEAVECHAT = 'LC',
+  MATCH = 'M',
+  UNMATCH = 'UM',
+  ROLLMATCH = 'RM',
+  SUCCESS = 'S',
+  ADDEMOTES = 'AE',
+  ERROR = 'ERR',
+  UNREGISTERED = 'UR',
+  TAKEABREAK = 'TAB'
+}
+
+export interface BASE {
+  userTwitch: {
+    name: string
+    id: string
+  }
+  channelTwitch: {
+    name: string
+    id: string
+  }
+  result?: any
+}
+
+export enum More {
+  NONE = 'NONE',
+  BIO = 'BIO',
+  FAVORITEEMOTES = 'FE',
+  FAVORITESTREAMERS = 'FS'
+}
+
+export interface ROLLMATCH extends BASE {
+  more: More
+}
+
+export interface UNMATCH extends BASE {
+  matchUserTwitch: {
+    name: string
+    id: string
+  }
+}
