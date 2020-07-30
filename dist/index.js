d3d8ac98e540101772e42a11db41845ef85fb04d
Karar Al-Remahy <alremahykarar@ymail.com>
2020-07-30 02:08:18 +0200

    fix: And not forgetting to publish the dist, too. I need sleep.

--- dist/index.js
+++ dist/index.js
@@ -11,3 +11,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 exports.__esModule = true;
 __exportStar(require("./MessageType"), exports);
+__exportStar(require("./TwitchLogin"), exports);
