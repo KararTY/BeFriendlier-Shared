86798842c2bb9e68090f3119bc155269a0589b67
Karar Al-Remahy <alremahykarar@ymail.com>
2020-07-30 03:50:07 +0200

    feat: Add requestAppToken.

--- src/TwitchAuth.ts
+++ src/TwitchAuth.ts
@@ -17,7 +17,7 @@ export interface TwitchAuthBody {
   access_token: string
   refresh_token: string
   expires_in: number
-  scope?: string
+  scope: string[]
   token_type: string
 }
 
@@ -25,7 +25,7 @@ export interface TwitchValidateBody {
   client_id: string
   expires_in: number
   login: string
-  scopes: string[]
+  scope: string[]
   user_id: string
 }
 
@@ -33,7 +33,7 @@ interface Config {
   clientToken: string
   clientSecret: string
   redirectURI: string
-  scopes: string[]
+  scope: string[]
   headers: Headers
 }
 
@@ -41,7 +41,7 @@ export class TwitchAuth {
   private readonly clientToken: string
   private readonly clientSecret: string
   private readonly redirectURI: string
-  private readonly scopes: string
+  private readonly scope: string
   private readonly headers: Headers
   private readonly logger: Logger
 
@@ -49,7 +49,7 @@ export class TwitchAuth {
     this.clientToken = config.clientToken
     this.clientSecret = config.clientSecret
     this.redirectURI = config.redirectURI
-    this.scopes = config.scopes.join(' ')
+    this.scope = config.scope.join(' ')
     this.headers = config.headers
     this.logger = new Logger({ enabled: true, name: 'befriendly-shared', level: loggerLevel })
   }
@@ -61,7 +61,7 @@ export class TwitchAuth {
       grant_type: 'authorization_code',
       code,
       redirect_uri: this.redirectURI,
-      scope: this.scopes,
+      scope: this.scope,
     }
 
     try {
@@ -73,6 +73,27 @@ export class TwitchAuth {
       return body
     } catch (error) {
       this.logger.error(null, 'Twitch.requestToken(): %O', error.response.body)
+      return null
+    }
+  }
+
+  public async requestAppToken (): Promise<TwitchAuthBody | null> {
+    const searchParams = {
+      client_id: this.clientToken,
+      client_secret: this.clientSecret,
+      grant_type: 'client_credentials',
+      scope: this.scope,
+    }
+
+    try {
+      const { body }: any = await fetch.post('https://id.twitch.tv/oauth2/token', {
+        headers: { ...this.headers },
+        searchParams,
+        responseType: 'json',
+      })
+      return body
+    } catch (error) {
+      this.logger.error(null, 'Twitch.requestAppToken(): %O', error.response.body)
       return null
     }
   }
@@ -107,7 +128,7 @@ export class TwitchAuth {
       client_secret: this.clientSecret,
       grant_type: 'refresh_token',
       refresh_token: encodeURI(token), // Per https://dev.twitch.tv/docs/authentication/#refreshing-access-tokens:~:text=URL%20encode
-      scope: this.scopes,
+      scope: this.scope,
     }
 
     try {
@@ -147,7 +168,7 @@ export class TwitchAuth {
 
     url += `&client_id=${this.clientToken}`
     url += `&redirect_uri=${this.redirectURI}`
-    url += `&scope=${this.scopes}`
+    url += `&scope=${this.scope}`
     url += '&force_verify=true'
     url += `&state=${csrfToken}`
 
